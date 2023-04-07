d4c57535d9103c02429c0d00873b61cd0a6db277
donkeygeek <donkeygeek1@gmail.com>
2023-04-07 19:37:56 +0200

    BrowserRouter, Route, Switch, Nav, NavLink, Strict et Exact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Community from './components/Community';
+import Docs from './components/Docs';
+import ErrorPage from './components/ErrorPage';
+import Menu from './components/Menu';
+import Tutorials from './components/Tutorials';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={Docs} />
+        <Route path="/tutorial" component={Tutorials} />
+        <Route strict path="/community" component={Community} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
