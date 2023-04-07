2a72ee2c58eec7403c763f5d8889afe4db278e42
donkeygeek <donkeygeek1@gmail.com>
2023-04-07 19:39:36 +0200

    Les redirections: Redirection via Redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Community from './components/Community';
 import Docs from './components/Docs';
@@ -6,18 +7,37 @@ import ErrorPage from './components/ErrorPage';
 import Menu from './components/Menu';
 import Tutorials from './components/Tutorials';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Menu />
-      <Switch>
-        <Route exact path="/" component={Docs} />
-        <Route path="/tutorial" component={Tutorials} />
-        <Route strict path="/community" component={Community} />
-        <Route component={ErrorPage} />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      underConst: {
+        Docs: false,
+        Tutorials: true,
+        Community: false
+      }
+    }
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Menu />
+        <Switch>
+          <Route exact path="/" component={Docs} />
+          {/* <Route path="/tutorial" component={Tutorials} /> */}
+          <Route path="/tutorial" render={() => (
+            this.state.underConst.Tutorials ? (<Redirect to="/" />) : (<Tutorials />)
+
+          )} />
+          <Route strict path="/community" component={Community} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
